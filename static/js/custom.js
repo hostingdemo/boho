870b81b840a5998fb48898f99939b733f7c15386
pratikgosavii <pratikgosavi654@gmail.com>
2021-08-10 11:30:53 +0530

    updated code

--- static/js/custom.js
+++ static/js/custom.js
@@ -56,34 +56,6 @@ window.addEventListener("load", function() {
 //js loader ---------end---------------
 
 
-var alert_items = document.querySelectorAll(".alert_item");
-var btns = document.querySelectorAll(".btn");
-var alert_wrapper = document.querySelector(".alert_wrapper");
-var close_btns = document.querySelectorAll(".close");
-
-btns.forEach(function(btn, btn_index) {
-    btn.addEventListener("click", function() {
-        alert_wrapper.classList.add("active");
-
-        alert_items.forEach(function(alert_item, alert_index) {
-            if (btn_index == alert_index) {
-                alert_item.style.top = "50%";
-            } else {
-                alert_item.style.top = "-100%";
-            }
-        })
-    })
-})
-
-close_btns.forEach(function(close, close_index) {
-    close.addEventListener("click", function() {
-        alert_wrapper.classList.remove("active");
-
-        alert_items.forEach(function(alert_item, alert_index) {
-            alert_item.style.top = "-100%";
-        })
-    })
-})
 
 
 
@@ -133,7 +105,7 @@ close_btns.forEach(function(close, close_index) {
 
 
 
-//dont know
+//dont know may be for categoriews
 $(".Click-here").on('click', function() {
     $(".custom-model-main").addClass('model-open');
 });
